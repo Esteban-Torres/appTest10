9b3e54711be8ec13adbfdd3152fc1dc81d266437
Esteban-Torres <esteban.garcia2295@gmail.com>
2019-10-23 09:15:19 -0500

    Actualizacion

--- djangoProjects/messenger/messenger/config/core.py
+++ djangoProjects/messenger/messenger/config/core.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.main',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'messenger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- djangoProjects/messenger/messenger/config/dev.py
+++ djangoProjects/messenger/messenger/config/dev.py
@@ -19,3 +19,6 @@ DATABASES = {
         'PORT': '5432', #Empty -> Default:5432
     }
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
